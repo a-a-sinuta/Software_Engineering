2a923956f11c4c8abff42398c0feb1219d7274e2
a.a.sinuta <a.a.sinuta@gmail.com>
2023-12-01 23:39:00 +0500

    add lab9_1

---
+++ lab9_1.py
@@ -0,0 +1,7 @@
+class Ivan:
+    __slots__ = ['name']
+    def __init__(self, name):
+            if name == 'Иван'
+                self.name = f"Да, я {name}"
+            else:
+                self.name = f''
