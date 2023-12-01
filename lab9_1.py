e8f0c18b3e50ee3d1eb110841ab6ad78428d5a5d
sashk <a.a.sinuta@gmail.com>
2023-12-02 04:19:14 +0500

    изменил readme файл

--- lab9_1.py
+++ lab9_1.py
@@ -1,7 +1,14 @@
 class Ivan:
     __slots__ = ['name']
     def __init__(self, name):
-            if name == 'Иван'
+            if name == 'Иван':
                 self.name = f"Да, я {name}"
             else:
-                self.name = f''
+                self.name = f'Я не {name}, а я Иван'
+
+person1 = Ivan('Алексей')
+person2 = Ivan('Иван')
+print(person1.name)
+print(person2.name)
+
+person2.surname = 'Петров'
